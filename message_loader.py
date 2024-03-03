6c899cfb5a81c8b09691e45960c8ac9cda55d75f
Skueletor <73276191+Skueletor@users.noreply.github.com>
2024-03-02 22:07:22 -0400

    Update message_loader.py

--- message_loader.py
+++ message_loader.py
@@ -1,1 +1,10 @@
+import json
 
+def load_messages(language):
+    try:
+        with open(f'langs/{language}.json', 'r', encoding='utf-8') as file:
+            messages = json.load(file)
+    except FileNotFoundError:
+        with open('langs/en.json', 'r', encoding='utf-8') as file:
+            messages = json.load(file)
+    return messages
